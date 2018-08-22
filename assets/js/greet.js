c215459664ab9f61781df2bd475509236008d3e6
josh581 <josh@deltstl.com>
2018-08-22 11:29:34 -0500

    pushing to github

--- assets/js/greet.js
+++ assets/js/greet.js
@@ -1,22 +1,4 @@
 // assets/js/greet.js
 export default function(name) {
-  // return `Yo yo ${name} - welcome to Encore!`;
-  // here
-  var peaks = ["Tallac", "Ralston", "Rose"];
-  var [last] = [...peaks].reverse();
-  console.log(last);
-  console.log(peaks.join(', '));
-  // here
-  var morning = {
-    breakfast: "oatmeal",
-    lunch: "peanut butter and jelly"
-  }
-
-  var dinner = "mac and cheese"
-
-  var backpackingMeals = {
-    ...morning,
-    dinner
-  }
-  console.log(backpackingMeals);
+  return `Yo yo ${name} - welcome to Encore!`;
 };
